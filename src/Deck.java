6a3f92f1503e02549704a7f09a25a95f3ad6e0c8
lucybrooks <lucybrooks@users.noreply.github.com>
2015-03-09 19:19:48 -0700

    Update Deck.java

--- src/Deck.java
+++ src/Deck.java
@@ -84,3 +84,37 @@ public class Deck {
      return cards.remove(0);
    }
  }
+/**
+  * Generates and returns a string representation of this deck.
+  * @return a string representation of this deck.
+  */
+ @Override
+ public String toString() {
+  String rtn = "size = " + size + "\nUndealt cards: \n";
+
+  for (int k = size - 1; k >= 0; k--) {
+   rtn = rtn + cards.get(k);
+   if (k != 0) {
+    rtn = rtn + ", ";
+   }
+   if ((size - k) % 2 == 0) {
+    // Insert carriage returns so entire deck is visible on console.
+    rtn = rtn + "\n";
+   }
+  }
+
+  rtn = rtn + "\nDealt cards: \n";
+  for (int k = cards.size() - 1; k >= size; k--) {
+   rtn = rtn + cards.get(k);
+   if (k != size) {
+    rtn = rtn + ", ";
+   }
+   if ((k - cards.size()) % 2 == 0) {
+    // Insert carriage returns so entire deck is visible on console.
+    rtn = rtn + "\n";
+   }
+  }
+
+  rtn = rtn + "\n";
+  return rtn;
+ }
